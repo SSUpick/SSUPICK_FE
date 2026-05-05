8878b5fbfa509f637fe7d97712b75f30f9fff53a
ParkSumin <fhsjdvs@gmail.com>
2026-05-05 20:15:18 +0900

    INITIAL SETTING

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="flex flex-col min-h-svh">
+      <h1 className="text-2xl font-bold text-center py-10">SSUPICK</h1>
+    </main>
+  )
+}
+
+export default App
