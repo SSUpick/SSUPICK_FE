836398978f5ed34bc86464952764077b5b5ba9cb
hyun907 <seunghyun020907@naver.com>
2026-05-10 23:56:54 +0900

    style: 글씨 굵기, 온보딩 대화창 투명도 변경

--- src/components/feedback/DialogBubble.tsx
+++ src/components/feedback/DialogBubble.tsx
@@ -6,7 +6,7 @@ type DialogBubbleProps = {
 
 export function DialogBubble({ children }: DialogBubbleProps) {
     return (
-        <div className="dialog-bubble-gradient-border text-black-800 backdrop-blur-bubble flex h-75 w-full items-center justify-center rounded-14 px-20 text-center text-lg leading-22 font-semibold tracking-tight">
+        <div className="dialog-bubble-gradient-border text-black-800 backdrop-blur-bubble flex h-75 w-full items-center justify-center rounded-14 px-20 text-center text-lg leading-22 font-medium tracking-tight">
             {children}
         </div>
     );
